99d60ea6134a82ed9cbc45e802bc85132040a474
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-12-24 20:23:02 -0500

    start d4

---
+++ day4/index.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from "fs";
+
+type BingoData = {
+  boards: number[][][];
+  drawnNumbers: number[];
+};
+
+const BOARD_SIZE = 5;
+
+const readInput = (file: string): BingoData => {
+  let data = readFileSync(file, "utf-8")
+    .split("\n")
+    .filter((l) => l !== "");
+
+  const drawnNumbers = data[0].split(",").map((n) => parseInt(n));
+
+  const boards: number[][][] = [];
+
+  data = data.slice(1);
+
+  for (let i = 0; i < data.length; i += BOARD_SIZE) {
+    const boardChunk = data.slice(i, i + BOARD_SIZE);
+    const board: number[][] = [];
+
+    boardChunk.forEach((line) =>
+      board.push(
+        line
+          .trim()
+          .split(" ")
+          .filter((c) => c !== "")
+          .map((n) => parseInt(n)),
+      ),
+    );
+
+    boards.push(board);
+  }
+
+  return { boards: boards, drawnNumbers: drawnNumbers };
+};
+
+const { boards, drawnNumbers } = readInput("./resources/input.txt");
+
+console.log(boards);
+console.log(drawnNumbers);
