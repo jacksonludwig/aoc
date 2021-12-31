e065138c1a3d7c6edbbedb7173631c1ef9570a5c
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-12-31 18:06:42 -0500

    format

--- day5/main.c
+++ day5/main.c
@@ -21,10 +21,12 @@ Line parse_single_line(char *line) {
   // Parse right side
   char *right = strtok(NULL, half_split);
 
-  Position start_pos = { atoi(strtok(left, xy_split)),  atoi(strtok(NULL, xy_split)) };
-  Position end_pos = { atoi(strtok(right, xy_split)),  atoi(strtok(NULL, xy_split)) };
+  Position start_pos = {atoi(strtok(left, xy_split)),
+                        atoi(strtok(NULL, xy_split))};
+  Position end_pos = {atoi(strtok(right, xy_split)),
+                      atoi(strtok(NULL, xy_split))};
 
-  Line parsed_line = { start_pos, end_pos };
+  Line parsed_line = {start_pos, end_pos};
 
   return parsed_line;
 }
@@ -34,7 +36,7 @@ void print_line(Line *line) {
   printf("End: %d, %d\n", line->end.x, line->end.y);
 }
 
-char** read_input(char *filename, unsigned int *line_amount) {
+char **read_input(char *filename, unsigned int *line_amount) {
   FILE *fp = fopen(filename, "r");
   char *line = NULL;
   size_t line_len = 0;
@@ -44,11 +46,12 @@ char** read_input(char *filename, unsigned int *line_amount) {
   size_t col_size = 1000;
 
   // Allocate space on heap for array of strings using sizes above
-  char **lines = malloc(row_size * sizeof(char*));
+  char **lines = malloc(row_size * sizeof(char *));
   for (int i = 0; i < row_size; i++)
     lines[i] = malloc((col_size + 1) * sizeof(char));
 
-  if (fp == NULL) exit(EXIT_FAILURE);
+  if (fp == NULL)
+    exit(EXIT_FAILURE);
 
   unsigned int index = 0;
   while ((read = getline(&line, &line_len, fp)) != -1) {
@@ -57,15 +60,16 @@ char** read_input(char *filename, unsigned int *line_amount) {
   }
 
   fclose(fp);
-  if (line) free(line);
+  if (line)
+    free(line);
 
   *line_amount = index;
   return lines;
 }
 
 void process_lines(char **lines, unsigned int amount_of_lines) {
-  for(int i = 0; i < amount_of_lines; i++) {
-    Line line = parse_single_line(lines[i]) ;
+  for (int i = 0; i < amount_of_lines; i++) {
+    Line line = parse_single_line(lines[i]);
     print_line(&line);
   }
 }
@@ -76,7 +80,7 @@ int main() {
 
   process_lines(lines, amount_of_lines);
 
-  for(int i = 0; i < amount_of_lines; i++) {
+  for (int i = 0; i < amount_of_lines; i++) {
     free(lines[i]);
   }
 
